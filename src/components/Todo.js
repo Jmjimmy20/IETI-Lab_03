a9a49b49e9eb507db50d146d17fe5bdb3aa5f3c3
Jmjimmy20 <Jmjimmy20@gmail.com>
2020-09-03 12:28:29 -0500

    ignore

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,6 +2,11 @@ import React from 'react';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import MomentUtils from '@date-io/moment';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+
 import './Todo.css';
 
 export class Todo extends React.Component {
@@ -13,13 +18,23 @@ export class Todo extends React.Component {
     render() {
         return (
 
-            <TableRow key={this.props.name}>
+            <Grid item xs={12} sm={6} md={3} mt={3} className="grid-item" >
+                <Card className="card">
+                    <CardContent>
+                        <Typography>{this.props.text}</Typography>
+                        <Typography>{this.props.priority}</Typography>
+                        <Typography>{this.props.dueDate.format('DD-MM-YYYY')}</Typography>
+                    </CardContent>
+                </Card>
+            </Grid>
+        );
+            {/*<TableRow key={this.props.name}>
                 <TableCell align="center">{this.props.text}</TableCell>
                 <TableCell align="center">{this.props.priority}</TableCell>
                 <TableCell align="center">{this.props.dueDate.format('DD/MM/YYYY')}</TableCell>
-            </TableRow>
+        </TableRow>*/}
             
-        );
+        
     }
 
 }
